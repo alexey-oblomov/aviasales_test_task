739ca258fb0c604768098337969c79b5f83dfd9c
Alexey Oblomov <alexey.oblomov@mail.ru>
2020-04-01 15:45:06 +0300

    Updates

---
+++ precache-manifest.730360d2c8b9eacf14ec3920a3364ddf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58a8adf2d34f16413b554243581b0157",
+    "url": "/alexey-oblomov/aviasales_test_task/index.html"
+  },
+  {
+    "revision": "1b2ed0a9e865593a4ad2",
+    "url": "/alexey-oblomov/aviasales_test_task/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1b0ff2102b472af041b1",
+    "url": "/alexey-oblomov/aviasales_test_task/static/js/2.d0eb3d98.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/alexey-oblomov/aviasales_test_task/static/js/2.d0eb3d98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b2ed0a9e865593a4ad2",
+    "url": "/alexey-oblomov/aviasales_test_task/static/js/main.7c3ca619.chunk.js"
+  },
+  {
+    "revision": "5638c55ba77bfc25d561",
+    "url": "/alexey-oblomov/aviasales_test_task/static/js/runtime-main.418d5ce0.js"
+  }
+]);
